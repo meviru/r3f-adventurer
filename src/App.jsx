aa89ae59293b17b4f21600953f611504b600c6d8
Viral Patel <viral.patel@1rivet.com>
2024-05-24 19:03:49 +0530

    feat: r3f - adventurer - initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,8 @@
 import { Canvas } from "@react-three/fiber";
 import { Experience } from "./components/Experience";
-
 function App() {
   return (
-    <Canvas shadows camera={{ position: [3, 3, 3], fov: 30 }}>
+    <Canvas shadows camera={{ position: [0, 2, 5], fov: 30 }}>
       <color attach="background" args={["#ececec"]} />
       <Experience />
     </Canvas>
--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -1,13 +1,31 @@
 import { OrbitControls } from "@react-three/drei";
+import { Adventurer } from "./Adventurer";
+import { useGLTF } from '@react-three/drei'
+import { useControls } from "leva";
+import { useEffect, useState } from "react";
 
 export const Experience = () => {
+  const { nodes, materials, animations: allAnimations } = useGLTF('models/Adventurer.glb')
+  const [animationsList, setAnimationsList] = useState([]);
+
+  useEffect(() => {
+    const result = allAnimations.map((animation) => animation.name.split("|").pop());
+    setAnimationsList(result);
+  }, [])
+
+  const { animation } = useControls({
+    animation: {
+      value: "Idle",
+      options: animationsList
+    }
+  }, [animationsList])
   return (
     <>
       <OrbitControls />
-      <mesh>
-        <boxGeometry />
-        <meshNormalMaterial />
-      </mesh>
+      <group position-y={-1}>
+        <Adventurer animation={animation} nodes={nodes} materials={materials} allAnimations={allAnimations} />
+      </group>
+      <ambientLight intensity={1} />
     </>
   );
 };
